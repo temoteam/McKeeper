c9f2a8b6a8d856718c4dbd507a4ddc3233cd2e2c
Sergey <nesenenko@temoteam.ru>
2019-03-08 16:55:57 +0300

    add info page

---
+++ app/src/main/java/com/temoteam/mckeeper/InfoFragment.java
@@ -0,0 +1,94 @@
+package com.temoteam.mckeeper;
+
+
+import android.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class InfoFragment extends Fragment {
+
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false);
+
+
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button mail = view.findViewById(R.id.button_mail);
+        Button tg = view.findViewById(R.id.button_tg);
+        Button vk = view.findViewById(R.id.button_vk);
+
+
+        mail.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        aaaa();
+                    }
+                }
+        );
+
+        tg.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        cccc();
+                    }
+                }
+        );
+
+        vk.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bbbb();
+                    }
+                }
+        );
+    }
+
+    private void aaaa() {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:nesenenko@temoteam.ru"));
+        try {
+            startActivity(emailIntent);
+        } catch (ActivityNotFoundException e) {
+            //TODO: Handle case where no email app is available
+        }
+    }
+
+    private void bbbb() {
+        String url = "https://vk.com/id241199506";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
+    private void cccc() {
+        String url = "https://t.me/lohness";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
+
+}
